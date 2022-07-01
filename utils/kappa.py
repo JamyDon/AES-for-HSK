ae8f349096179380da3386fc1d45909478b5a195
JamyDon <2000013076@stu.pku.edu.cn>
2022-07-01 14:23:09 +0800

    update kappa.py

--- utils/kappa.py
+++ utils/kappa.py
@@ -1,10 +1,6 @@
-import numpy as np
+from sklearn.metrics import cohen_kappa_score
 
 
-def kappa(confusion_matrix):
-    rows = np.sum(confusion_matrix, axis=0)
-    cols = np.sum(confusion_matrix, axis=1)
-    total_sum = sum(cols)
-    p_e = np.dot(rows, cols) / float(total_sum ** 2)
-    p_o = np.trace(confusion_matrix) / float(total_sum)
-    return (p_o - p_e) / (1 - p_e)
+def kappa(y_true, y_pred):
+    kappa_value = cohen_kappa_score(y_true, y_pred)
+    return kappa_value
